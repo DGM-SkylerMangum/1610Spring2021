5bbab18d788fd57296cf9c1562876f78ac107f4d
Skyler Mangum <10900665@my.uvu.edu>
2021-02-05 13:02:10 -0700

    Create Ball Generator
    Got a working ball generator in game

---
+++ UnityStuff/2DGame/First2DGame/Assets/BallGenerator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject ball;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(ball);
+        }
+    }
+
+}
