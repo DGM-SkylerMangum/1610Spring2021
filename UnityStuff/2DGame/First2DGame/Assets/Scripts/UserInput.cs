3d5dc9c9582e55151c25230170ab4d3976b61c2b
Skyler Mangum <10900665@my.uvu.edu>
2021-02-05 22:05:58 -0700

    Screwing Around With Axis and Position
    Didn't necessarily work with 2d environment but I'm starting to understand the syntax and inputs.

--- UnityStuff/2DGame/First2DGame/Assets/Scripts/UserInput.cs
+++ UnityStuff/2DGame/First2DGame/Assets/Scripts/UserInput.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
-    public Rigidbody2D ballRigidbody2D;
-    public float moveSpeed = 5f;
-    public float jumping = 10f;
-    private Vector2 sideMovement;
-    private Vector2 jumpMovement;
+    public float range;
 
     void Update()
     {
-        sideMovement.x = Input.GetAxis("Horizontal") * moveSpeed;
-        if (!Input.GetButtonDown("Jump")) return;
-        jumpMovement.y = jumping;
-    }
-    void FixedUpdate()
-    {
-        
+        float moveSpeed = Input.GetAxis("Horizontal");
+        float xPos = moveSpeed * range;
+    transform.position = new Vector3(xPos, 0, 0);
     }
 
 }
