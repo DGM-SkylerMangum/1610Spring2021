0f8be0adcc0e4d2f9fcfe3e72a535114e8e8398b
Skyler Mangum <10900665@my.uvu.edu>
2021-02-12 10:37:25 -0700

    Modified VariablesAndFunctions
    this fits variables and operators as I explored some more editing for the processes for variables and also practiced creating a function within a class

--- UnityStuff/2DGame/First2DGame/Assets/Scripts/VariablesAndFunctions.cs
+++ UnityStuff/2DGame/First2DGame/Assets/Scripts/VariablesAndFunctions.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VariablesAndFunctions : MonoBehaviour
 {
     public int myInt = 5;
+    public int mySecondInt = 10;
     // Start is called before the first frame update
     int MultiplyByTwo(int number)
     {
@@ -12,9 +13,17 @@ public class VariablesAndFunctions : MonoBehaviour
         ret = number * 2;
         return ret;
     }
+    int AddingTheOtherInt(int number, int otherNum)
+    {
+        int ret;
+        number += otherNum;
+        ret = number;
+        return ret;
+    }
     void Start()
     {
         myInt = MultiplyByTwo(myInt);
+        myInt = AddingTheOtherInt(myInt, mySecondInt);
         Debug.Log(myInt);
     }
 }
