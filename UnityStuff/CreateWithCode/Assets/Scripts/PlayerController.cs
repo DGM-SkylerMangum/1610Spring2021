d671c2de2a2eed2fc83ebe6b53561b30b4e728df
Skyler Mangum <10900665@my.uvu.edu>
2021-03-05 11:41:30 -0700

    Modified files so it completes Step Into the Drivers Seat
    added rotation instead of sliding and modified variables for mass

--- UnityStuff/CreateWithCode/Assets/Scripts/PlayerController.cs
+++ UnityStuff/CreateWithCode/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,19 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    public float speed = 5.0f;
+    private float speed = 2.0f;
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Update is called once per frame
     void Update()
     {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * 20 * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20 * speed * forwardInput);
+        //rotates
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput* Time.deltaTime);
     }
 }
