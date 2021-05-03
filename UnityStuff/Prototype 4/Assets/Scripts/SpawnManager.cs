d951a8892f8df69965a6441e05a22be3807bbdf9
Skyler Mangum <10900665@my.uvu.edu>
2021-05-03 17:08:53 -0600

    Finished Unit 4.2
    movement of enemy, enemy spawning, enemy following

---
+++ UnityStuff/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosY);
+        return randomPos;
+    }
+}
