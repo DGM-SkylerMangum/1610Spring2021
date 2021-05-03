65f4b68218c42d7996a9d6aafdfac89cef1b099a
Skyler Mangum <10900665@my.uvu.edu>
2021-05-03 14:56:04 -0600

    Finished 3.2 Unit
    Got working functionality of the game. Got instantiation of obstacles, and the movement to stop when the game ends

--- UnityStuff/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ UnityStuff/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -8,15 +8,20 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
-    private float repeatRate = 20;
+    private float repeatRate = 1;
+    private PlayerController playerControllerScript;
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
