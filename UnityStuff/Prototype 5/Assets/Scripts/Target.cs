e9b1008295ef96179c0b041fc322600adf086e62
Skyler Mangum <10900665@my.uvu.edu>
2021-05-03 20:30:13 -0600

    5.2 completed
    Got some trouble with prefab particles, but eventually got the inspector working with the project viewer. Pretty straightforward addition

--- UnityStuff/Prototype 5/Assets/Scripts/Target.cs
+++ UnityStuff/Prototype 5/Assets/Scripts/Target.cs
@@ -11,12 +11,16 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    private GameManager gameManager;
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -38,7 +42,9 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
-            Destroy(gameObject);
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
