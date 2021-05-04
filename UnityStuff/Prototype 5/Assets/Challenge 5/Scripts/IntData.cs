ccdee468adaf669a64700c84e9aa2278aed67b7f
Skyler Mangum <10900665@my.uvu.edu>
2021-05-03 23:28:28 -0600

    Finished up any last additions of missing code
    also added any final discussion responses needed in canvas

---
+++ UnityStuff/Prototype 5/Assets/Challenge 5/Scripts/IntData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    // Start is called before the first frame update
+    protected int value;
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+
+    // Update is called once per frame
+    public void ReplaceValue(int number)
+    {
+        value = number;
+    }
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = value;
+    }
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value.ToString();
+    }
+}
