0de324a7223c2f4f5b7932b0228e525cb9ba233e
Skyler Mangum <10900665@my.uvu.edu>
2021-05-03 23:08:00 -0600

    Unit 15
    looked into other programming skills with Unity Events. The usefulness of calling multiple actions for one interaction

---
+++ UnityStuff/Prototype 5/Assets/Challenge 5/Scripts/UnityEventBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventBehaviour : MonoBehaviour
+{
+    [SerializeField] private UnityEvent myTrigger;
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            myTrigger.Invoke();
+        }
+    }
+}
